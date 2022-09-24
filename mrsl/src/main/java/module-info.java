135a0476092f4cdff0e3e30e0054ddf7b71c5e6d
gnahraf <farhang@gmail.com>
2022-09-24 13:16:46 -0600

    build conf + doc'ing how to for native builds

--- mrsl/src/main/java/module-info.java
+++ mrsl/src/main/java/module-info.java
@@ -4,7 +4,5 @@ module io.crums.sldg.mrsl_cli {
   requires io.crums.util;
   requires io.crums.sldg.reports.pdf;
   requires info.picocli;
-  
-  
-  exports io.crums.sldg.cli.mrsl;
+  opens io.crums.sldg.cli.mrsl to info.picocli;
 }
